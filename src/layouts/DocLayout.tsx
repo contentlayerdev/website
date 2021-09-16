ac7a93f3fc72bbb45a829816c85282e1c832dd34
Sean C Davis <scdavis41@gmail.com>
2021-09-15 22:30:23 -0400

    Add a rough list of all docs for navigation

--- src/layouts/DocLayout.tsx
+++ src/layouts/DocLayout.tsx
@@ -1,11 +1,24 @@
 import type { FC } from "react";
 
 import { Layout } from "../components/Layout";
+import { allDocs } from ".contentlayer/data";
 import type * as types from ".contentlayer/types";
 
 export const DocLayout: FC<{ doc: types.Doc }> = ({ doc }) => {
   return (
     <Layout doc={doc}>
+      <div>
+        <h2>All Docs</h2>
+        <ul>
+          {allDocs.map((doc, idx) => {
+            return (
+              <li key={idx}>
+                <a href={`/${doc.url_path}`}>{doc.title}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
       <h1>{doc.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: doc.body.html }} />
     </Layout>
