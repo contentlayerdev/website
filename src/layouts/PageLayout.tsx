3d14933551cb2cd344c8558bf0362d2612584cdc
Sean C Davis <scdavis41@gmail.com>
2021-09-20 21:01:39 -0400

    Clean up the basic page layout

--- src/layouts/PageLayout.tsx
+++ src/layouts/PageLayout.tsx
@@ -6,8 +6,10 @@ import type * as types from ".contentlayer/types";
 export const PageLayout: FC<{ page: types.Page }> = ({ page }) => {
   return (
     <Layout doc={page}>
-      <h1>{page.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: page.body.html }} />
+      <div className="max-w-2xl mx-auto py-8">
+        <h1>{page.title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: page.body.html }} />
+      </div>
     </Layout>
   );
 };
