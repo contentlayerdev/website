775ac7ad1c50255302ddcf20f21b084efbba725a
Sean C Davis <scdavis41@gmail.com>
2021-12-08 15:49:35 -0500

    Add option for a "nav_title" field to override the label in the sidebar

--- src/pages/docs/[[...slug]].tsx
+++ src/pages/docs/[[...slug]].tsx
@@ -1,23 +1,28 @@
-import type { InferGetStaticPropsType } from 'next'
-import { useLiveReload } from 'next-contentlayer/hooks'
-import type { FC } from 'react'
-import { allDocs } from '.contentlayer/data'
-import { Doc } from '.contentlayer/types'
+import type { InferGetStaticPropsType } from "next";
+import { useLiveReload } from "next-contentlayer/hooks";
+import type { FC } from "react";
+import { allDocs } from ".contentlayer/data";
+import { Doc } from ".contentlayer/types";
 
-import { DocLayout } from '../../layouts/DocLayout'
-import { defineStaticProps, toParams } from '../../utils/next'
+import { DocLayout } from "../../layouts/DocLayout";
+import { defineStaticProps, toParams } from "../../utils/next";
 
 export const getStaticPaths = async () => {
-  const paths = allDocs.map((_) => _.pathSegments.map((_: PathSegment) => _.pathName).join('/')).map(toParams)
+  const paths = allDocs
+    .map((_) => _.pathSegments.map((_: PathSegment) => _.pathName).join("/"))
+    .map(toParams);
 
-  return { paths, fallback: 'blocking' }
-}
+  return { paths, fallback: "blocking" };
+};
 
 export const getStaticProps = defineStaticProps(async (context) => {
-  const params = context.params as any
-  const pagePath = params.slug?.join('/') ?? ''
+  const params = context.params as any;
+  const pagePath = params.slug?.join("/") ?? "";
 
-  const doc = allDocs.find((_) => _.pathSegments.map((_: PathSegment) => _.pathName).join('/') === pagePath)!
+  const doc = allDocs.find(
+    (_) =>
+      _.pathSegments.map((_: PathSegment) => _.pathName).join("/") === pagePath
+  )!;
 
   // if (doc === undefined) {
   //   return {
@@ -25,31 +30,34 @@ export const getStaticProps = defineStaticProps(async (context) => {
   //   } as never // return as `never` to not confuse `InferGetStaticPropsType`
   // }
 
-  const tree = buildTree(allDocs)
+  const tree = buildTree(allDocs);
 
-  return { props: { doc, tree } }
-})
+  return { props: { doc, tree } };
+});
 
-const Page: FC<InferGetStaticPropsType<typeof getStaticProps>> = ({ doc, tree }) => {
-  useLiveReload()
+const Page: FC<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  doc,
+  tree,
+}) => {
+  useLiveReload();
 
-  return <DocLayout {...{ doc, tree }} />
-}
+  return <DocLayout {...{ doc, tree }} />;
+};
 
-export default Page
+export default Page;
 
-export type TreeRoot = TreeNode[]
+export type TreeRoot = TreeNode[];
 
 export type TreeNode = {
-  title: string
-  urlPath: string
-  children: TreeNode[]
-}
+  title: string;
+  urlPath: string;
+  children: TreeNode[];
+};
 
-type PathSegment = { order: number; pathName: string }
+type PathSegment = { order: number; pathName: string };
 
 const buildTree = (docs: Doc[], parentPathNames: string[] = []): TreeNode[] => {
-  const level = parentPathNames.length
+  const level = parentPathNames.length;
 
   return docs
     .filter(
@@ -57,16 +65,18 @@ const buildTree = (docs: Doc[], parentPathNames: string[] = []): TreeNode[] => {
         _.pathSegments.length === level + 1 &&
         _.pathSegments
           .map((_: PathSegment) => _.pathName)
-          .join('/')
-          .startsWith(parentPathNames.join('/')),
+          .join("/")
+          .startsWith(parentPathNames.join("/"))
     )
     .sort((a, b) => a.pathSegments[level].order - b.pathSegments[level].order)
     .map<TreeNode>((doc) => ({
-      title: doc.title,
-      urlPath: '/docs/' + doc.pathSegments.map((_: PathSegment) => _.pathName).join('/'),
+      title: doc.nav_title || doc.title,
+      urlPath:
+        "/docs/" +
+        doc.pathSegments.map((_: PathSegment) => _.pathName).join("/"),
       children: buildTree(
         docs,
-        doc.pathSegments.map((_: PathSegment) => _.pathName),
+        doc.pathSegments.map((_: PathSegment) => _.pathName)
       ),
-    }))
-}
+    }));
+};
