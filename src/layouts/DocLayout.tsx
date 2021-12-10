4c7d53fccecd56c493c5e4f596abd31d1a8d3822
Johannes Schickling <schickling.j@gmail.com>
2021-12-10 11:00:15 +0000

    darkmode and other improvements

--- src/layouts/DocLayout.tsx
+++ src/layouts/DocLayout.tsx
@@ -1,54 +1,76 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import type { FC } from 'react'
-import type * as types from '.contentlayer/types'
-import classnames from 'classnames'
+import Link from "next/link";
+import { useRouter } from "next/router";
+import type { FC } from "react";
+import type * as types from ".contentlayer/types";
+import classnames from "classnames";
 
-import { Layout } from '../components/Layout'
-import { TreeRoot } from '../pages/docs/[[...slug]]'
+import { Layout } from "../components/Layout";
+import { TreeRoot } from "../pages/docs/[[...slug]]";
+import React from "react";
 
-export const DocLayout: FC<{ doc: types.Doc; tree: TreeRoot }> = ({ doc, tree }) => {
-  const router = useRouter()
-  const SIDEBAR_WIDTH = 320
-  const HEADER_HEIGHT = 60
+export const DocLayout: FC<{ doc: types.Doc; tree: TreeRoot }> = ({
+  doc,
+  tree,
+}) => {
+  const router = useRouter();
+  const SIDEBAR_WIDTH = 320;
+  const HEADER_HEIGHT = 60;
 
   return (
     <Layout doc={doc}>
       <div className="flex">
         <aside
           className="fixed"
-          style={{ height: `calc(100vh - ${HEADER_HEIGHT}px)`, width: SIDEBAR_WIDTH, top: HEADER_HEIGHT }}
+          style={{
+            height: `calc(100vh - ${HEADER_HEIGHT}px)`,
+            width: SIDEBAR_WIDTH,
+            top: HEADER_HEIGHT,
+          }}
         >
-          <div className="h-full p-4 overflow-y-auto border-r">
+          <div className="h-full p-4 overflow-y-auto border-r border-gray-100 dark:border-gray-800">
             <Tree tree={tree} level={0} activeUrlPath={router.asPath} />
           </div>
         </aside>
-        <div className="flex-1 max-w-2xl px-12 py-8 markdown" style={{ marginLeft: SIDEBAR_WIDTH }}>
+        <div
+          className="flex-1 max-w-2xl px-12 py-8 markdown"
+          style={{ marginLeft: SIDEBAR_WIDTH }}
+        >
           <h1>{doc.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: doc.body.html }} />
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-const Tree: FC<{ tree: TreeRoot; level: number; activeUrlPath: string }> = ({ tree, level, activeUrlPath }) => (
+const Tree: FC<{ tree: TreeRoot; level: number; activeUrlPath: string }> = ({
+  tree,
+  level,
+  activeUrlPath,
+}) => (
   <div style={{ paddingLeft: level * 12 }} className="mb-2 space-y-1">
     {tree.map((treeNode) => (
-      <div key={treeNode.urlPath}>
+      <React.Fragment key={treeNode.urlPath}>
         <Link href={treeNode.urlPath}>
           <a
-            className={classnames('p-2 no-underline text-sm block rounded-md text-gray-800 hover:bg-gray-50', {
-              'bg-gray-100 font-bold': activeUrlPath === treeNode.urlPath,
-            })}
+            className={classnames(
+              "py-2 px-4 no-underline text-sm block rounded-md hover:bg-gray-50 dark:hover:bg-gray-850",
+              activeUrlPath === treeNode.urlPath
+                ? "bg-gray-100 text-black dark:bg-gray-800 dark:text-white"
+                : "text-gray-500 dark:text-gray-400"
+            )}
           >
             {treeNode.title}
           </a>
         </Link>
         {treeNode.children.length > 0 && (
-          <Tree tree={treeNode.children} level={level + 1} activeUrlPath={activeUrlPath} />
+          <Tree
+            tree={treeNode.children}
+            level={level + 1}
+            activeUrlPath={activeUrlPath}
+          />
         )}
-      </div>
+      </React.Fragment>
     ))}
   </div>
-)
+);
