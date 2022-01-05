82422b0d16d3d5a7f486971055f5e8e06796b9ce
Sean C Davis <scdavis41@gmail.com>
2022-01-05 16:18:50 -0500

    Add a bottom margin to callouts

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -5,7 +5,7 @@ export const Callout: FC = ({ children }) => {
   const markdownContent: string = children?.toString() ?? ''
 
   return (
-    <div className="border-l-4 border-blue-400 p-4 bg-gray-800 rounded-sm">
+    <div className="mb-4 border-l-4 border-blue-400 p-4 bg-gray-800 rounded-sm">
       <Markdown>{markdownContent}</Markdown>
     </div>
   )
