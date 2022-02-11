7be6076fc078f37c4f140652089916d5e599652a
Johannes Schickling <schickling.j@gmail.com>
2022-02-11 18:33:07 +0100

    update stackblitz

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -5,7 +5,7 @@ export const Playground: FC = () => {
     <div className="h-[800px] my-8 relative mx-[-50vw] right-1/2 left-1/2 w-screen px-5">
       <iframe
         className="w-full h-full"
-        src="https://stackblitz.com/edit/github-38v7yj-m5ynvg?devtoolsheight=33&embed=1&file=posts/post-01.md"
+        src="https://stackblitz.com/edit/github-jmeqhv?devtoolsheight=33&embed=1&file=posts/post-01.md"
       />
     </div>
   )
