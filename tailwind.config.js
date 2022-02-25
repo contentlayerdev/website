3030281f66c63c2ed974aec675ee3d9344606d33
Sean C Davis <scdavis41@gmail.com>
2022-02-25 15:43:04 -0500

    More global style adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,36 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './content/**/*.{md,mdx}'],
   theme: {
+    colors: {
+      black: '#000024',
+      blue: '#00D1FF',
+      white: '#ffffff',
+      lime: '#DDFF56',
+    },
     extend: {
       colors: {
-        cl: {
-          'gray-100': '#F6F6F6',
-          'gray-500': '#808080',
-          'gray-900': '#2C2C2C',
-          'gray-925': '#202124',
-          'gray-950': '#171717',
-          'gray-975': '#0F0F0F',
-          black: '#000000',
-          blue: '#00D1FF',
-          white: '#ffffff',
-          yellow: '#DDFF56',
-        },
         gray: {
-          850: '#18202F',
-          950: '#0b0f1a',
+          // defaults
+          50: '#f9fafb',
+          200: '#e5e7eb',
+          300: '#d1d5db',
+          400: '#9ca3af',
+          500: '#6b7280',
+          600: '#4b5563',
+          700: '#374151',
+          800: '#1f2937',
+          900: '#111827',
+          // overrides
+          100: '#F6F6F6',
+          500: '#808080',
+          900: '#2C2C2C',
+          925: '#202124',
+          950: '#171717',
+          975: '#0F0F0F',
         },
       },
       fontFamily: {
-        cl: [
+        sans: [
           'fkdisplay-regular',
           'ui-sans-serif',
           'system-ui',
