4c97b012d7c1122bfc822d47e6b09ca2cfaf8c67
Johannes Schickling <schickling.j@gmail.com>
2022-03-07 17:39:31 +0100

    improved snippets

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -9,8 +9,10 @@ export const LandingPage: React.FC<{ codeSnippetsHtml: CodeSnippets }> = ({ code
       <Header />
       <div className="h-full pt-[57px]">
         <div>Contentlayer</div>
-        <div dangerouslySetInnerHTML={{ __html: codeSnippetsHtml.step1['contentlayer.config.ts'] }} />
-        <div dangerouslySetInnerHTML={{ __html: codeSnippetsHtml.step3['pages/posts/[slug].tsx'] }} />
+        <div className="max-w-xl" style={{ fontSize: 13 }}>
+          <div dangerouslySetInnerHTML={{ __html: codeSnippetsHtml.step1['contentlayer.config.ts'] }} />
+          <div dangerouslySetInnerHTML={{ __html: codeSnippetsHtml.step3['pages/posts/[slug].tsx'] }} />
+        </div>
         <Playground />
       </div>
     </div>
