3e641f0b2490410a7512229f1ee4e28f1ded25d8
Lukas Wiesehan <mail@lukaswiesehan.de>
2022-03-15 11:20:43 +0100

    Address tweets section: slower carousel, link to tweet, show less on mobile

--- src/components/LandingPage/Tweets.tsx
+++ src/components/LandingPage/Tweets.tsx
@@ -1,5 +1,7 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+import Link from 'next/link'
 import { User } from '../User'
+import { Button } from '../Button'
 
 const content = {
   tweets: [
@@ -10,6 +12,7 @@ const content = {
         position: 'Developer Relations at Vercel',
         avatar: 'https://pbs.twimg.com/profile_images/1194080814688079872/6qhYKGKC_400x400.jpg',
       },
+      url: '/',
     },
     {
       text: 'I’m using Contentlayer on my own website and it just works. No going back. Finally it’s easy to use content in my apps.',
@@ -18,6 +21,7 @@ const content = {
         position: 'Creator of Radix UI',
         avatar: 'https://avatars.githubusercontent.com/u/372831?v=4',
       },
+      url: '/',
     },
     {
       text: 'I’m not using Contentlayer yet but I’m sure it’s great. Very excited about it in any case.',
@@ -26,6 +30,7 @@ const content = {
         position: 'Staff Designer at GitHub',
         avatar: 'https://pbs.twimg.com/profile_images/1217652661962661888/WfiUNjzP_400x400.jpg',
       },
+      url: '/',
     },
     {
       text: 'I’m using Contentlayer on my own website and it just works. No going back.',
@@ -34,30 +39,67 @@ const content = {
         position: 'Founder of Prisma',
         avatar: 'https://pbs.twimg.com/profile_images/1452580365156208644/MSsWd3oT_400x400.jpg',
       },
+      url: '/',
     },
   ],
 }
 
+const Tweet: FC<{ text: string; user: { name: string; position: string; avatar: string }; url: string }> = ({
+  text,
+  user,
+  url,
+}) => {
+  return (
+    <Link href={url}>
+      <a
+        className="block h-full bg-gray-50/50 border border-gray-100 rounded-2xl p-8 space-y-4 dark:bg-gray-900/50 dark:border-gray-900"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <User {...user} />
+        <p className="text-slate-500 font-light italic dark:text-slate-400">
+          <q>{text}</q>
+        </p>
+      </a>
+    </Link>
+  )
+}
+
 export const Tweets: FC = () => {
+  const [tweetsToShow, setTweetsToShow] = useState<number>(2)
   return (
     <div className="relative w-full mt-16 md:mt-24 lg:mt-32 overflow-x-hidden">
-      <ul className="list-none m-0 space-y-8 md:space-y-0 md:flex md:animate-scroll">
-        {[...content.tweets, ...content.tweets].map(({ text, user }, index) => (
+      <ul className="hidden md:flex list-none m-0 space-y-8 md:space-y-0 md:animate-scroll">
+        {[...content.tweets, ...content.tweets, ...content.tweets].map((tweet, index) => (
           <li
             key={index}
             className={`md:w-[560px] shrink-0 grow-0 px-4 ${
               index >= content.tweets.length ? 'hidden md:block' : 'block'
             }`}
           >
-            <div className="h-full bg-gray-50/50 border border-gray-100 rounded-2xl p-8 space-y-4 dark:bg-gray-900/50 dark:border-gray-900">
-              <User {...user} />
-              <p className="text-slate-500 font-light italic dark:text-slate-400">
-                <q>{text}</q>
-              </p>
-            </div>
+            <Tweet {...tweet} />
           </li>
         ))}
       </ul>
+      <div className="md:hidden">
+        <ul className="list-none m-0 space-y-8 md:space-y-0 md:animate-scroll">
+          {content.tweets.slice(0, tweetsToShow).map((tweet, index) => (
+            <li
+              key={index}
+              className={`md:w-[560px] shrink-0 grow-0 px-4 ${
+                index >= content.tweets.length ? 'hidden md:block' : 'block'
+              }`}
+            >
+              <Tweet {...tweet} />
+            </li>
+          ))}
+        </ul>
+        {tweetsToShow < content.tweets.length && (
+          <div className="w-full px-4 pt-8 flex justify-center">
+            <Button label="Show more" action={() => setTweetsToShow(tweetsToShow + 2)} theme="primary" />
+          </div>
+        )}
+      </div>
     </div>
   )
 }
