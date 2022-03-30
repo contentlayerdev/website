82499580c2e76b804632c9f636b03fcf70a080b7
Lukas Wiesehan <mail@lukaswiesehan.de>
2022-03-30 09:13:38 +0200

    Increase dark mode contrast in callout component

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -3,8 +3,8 @@ import { Icon } from './Icon'
 
 export const Callout: FC = ({ children }) => {
   return (
-    <div className="rounded-lg border border-violet-100 bg-violet-50 dark:border-violet-900/50 dark:bg-violet-900/25">
-      <div className="flex space-x-4 p-6 py-4 font-semibold text-violet-600 dark:text-violet-500">
+    <div className="rounded-lg border border-violet-100 bg-violet-50 dark:border-violet-900/50 dark:bg-violet-900/20">
+      <div className="flex space-x-4 p-6 py-4 font-semibold text-violet-600 dark:text-violet-400">
         <div className="mt-1 w-5 shrink-0 text-violet-500 dark:text-violet-700">
           <Icon name="exclamation" />
         </div>
