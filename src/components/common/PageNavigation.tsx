71e4b53a913bc508999747bd4e14726b51dcbf70
Sean C Davis <scdavis41@gmail.com>
2022-04-11 15:02:14 -0400

    Don't render "on this page" when there are no headings

--- src/components/common/PageNavigation.tsx
+++ src/components/common/PageNavigation.tsx
@@ -23,34 +23,36 @@ export const PageNavigation: FC<{ headings: DocHeading[] }> = ({ headings }) => 
     }
   }, [headings])
 
+  const headingsToRender = headings.filter((_) => _.level > 1)
+
+  if ((headingsToRender ?? []).length === 0) return null
+
   return (
     <div className="text-sm">
       <h4 className="mb-4 font-medium text-slate-600 dark:text-slate-300">On this page</h4>
       <ul className="space-y-2">
-        {headings
-          .filter((_) => _.level > 1)
-          .map(({ title, level }, index) => (
-            <li key={index}>
-              <a
-                href={`#${sluggifyTitle(getNodeText(title))}`}
-                style={{ marginLeft: (level - 2) * 16 }}
-                className={`flex ${
-                  sluggifyTitle(getNodeText(title)) == activeHeading
-                    ? 'text-violet-600 dark:text-violet-400'
-                    : 'hover:text-slate-600 dark:hover:text-slate-300'
-                }`}
-              >
-                <span className="mr-2 mt-[5px] block w-1.5 shrink-0">
-                  <Icon name="chevron-right" />
-                </span>
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: title.replace('`', '<code style="font-size: 0.75rem;">').replace('`', '</code>'),
-                  }}
-                />
-              </a>
-            </li>
-          ))}
+        {headingsToRender.map(({ title, level }, index) => (
+          <li key={index}>
+            <a
+              href={`#${sluggifyTitle(getNodeText(title))}`}
+              style={{ marginLeft: (level - 2) * 16 }}
+              className={`flex ${
+                sluggifyTitle(getNodeText(title)) == activeHeading
+                  ? 'text-violet-600 dark:text-violet-400'
+                  : 'hover:text-slate-600 dark:hover:text-slate-300'
+              }`}
+            >
+              <span className="mr-2 mt-[5px] block w-1.5 shrink-0">
+                <Icon name="chevron-right" />
+              </span>
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: title.replace('`', '<code style="font-size: 0.75rem;">').replace('`', '</code>'),
+                }}
+              />
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   )
