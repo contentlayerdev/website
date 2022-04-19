e852e16d2d565ae5a8b339f00845de97ada66bf1
Johannes Schickling <schickling.j@gmail.com>
2022-04-19 15:02:31 +0200

    WIP beta post + misc improvements

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -9,9 +9,14 @@ import { Heading } from './Heading'
 import { Checklist } from './Checklist'
 import { Paragraph } from './Paragraph'
 import { Video } from './Video'
+import Link from 'next/link'
 
 const content = {
   heading: 'Content made easy for developers',
+  recentBlogPost: {
+    title: 'Introducing Contentlayer (Beta): Content Made Easy for Developers',
+    url: '/blog/beta',
+  },
   text: (
     <>
       Contentlayer is a{' '}
@@ -40,19 +45,31 @@ export const Hero: FC = () => {
   const router = useRouter()
 
   return (
-    <div className="mx-auto grid w-full max-w-screen-xl grid-cols-1 gap-16 px-4 pt-8 md:grid-cols-2 md:px-8 md:pt-24 lg:pt-32">
-      <div className="space-y-8 sm:max-w-md">
-        <Heading level={1}>{content.heading}</Heading>
-        <Paragraph className="text-lg">{content.text}</Paragraph>
-        <Checklist items={content.features} className="text-lg" />
-        <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 md:flex-col md:space-y-4 md:space-x-0 lg:flex-row lg:space-y-0 lg:space-x-4">
-          <Button label={content.primaryAction.label} href={content.primaryAction.url} theme="primary" />
-          <Button label={content.secondaryAction.label} href={content.secondaryAction.url} theme="secondary" />
+    <div className="w-full space-y-20 pt-8 md:pt-16 lg:pt-20">
+      <div className="flex w-full justify-center">
+        <div className="flex items-center space-x-2.5 rounded-md border border-slate-200 bg-slate-50 py-3 px-4 dark:border-slate-700 dark:bg-slate-900">
+          <div className="rounded-md bg-purple-500 py-1 px-1.5 text-xs font-semibold uppercase text-white dark:bg-purple-800">
+            New
+          </div>
+          <Link href={content.recentBlogPost.url}>
+            <a className="text-sm font-semibold text-slate-700 dark:text-slate-100">{content.recentBlogPost.title}</a>
+          </Link>
         </div>
       </div>
-      <div className="relative flex w-full items-center">
-        {/* TODO don't use image as thumbnail if possible */}
-        <Video thumbnail={content.video.thumbnail} videoId={content.video.youtubeId} />
+      <div className="mx-auto grid w-full max-w-screen-xl grid-cols-1 gap-16 px-4 md:grid-cols-2 md:px-8 ">
+        <div className="space-y-8 sm:max-w-md">
+          <Heading level={1}>{content.heading}</Heading>
+          <Paragraph className="text-lg">{content.text}</Paragraph>
+          <Checklist items={content.features} className="text-lg" />
+          <div className="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4 md:flex-col md:space-y-4 md:space-x-0 lg:flex-row lg:space-y-0 lg:space-x-4">
+            <Button label={content.primaryAction.label} href={content.primaryAction.url} theme="primary" />
+            <Button label={content.secondaryAction.label} href={content.secondaryAction.url} theme="secondary" />
+          </div>
+        </div>
+        <div className="relative flex w-full items-center">
+          {/* TODO don't use image as thumbnail if possible */}
+          <Video thumbnail={content.video.thumbnail} videoId={content.video.youtubeId} />
+        </div>
       </div>
     </div>
   )
