6b2bee2ae4fdb71d810f7e92280de06052b7d6b1
Johannes Schickling <schickling.j@gmail.com>
2022-04-19 17:01:50 +0200

    WIP post

--- src/components/landing-page/Support.tsx
+++ src/components/landing-page/Support.tsx
@@ -68,16 +68,16 @@ const content = {
 
 export const Support: FC = () => {
   return (
-    <div className="w-full max-w-screen-xl px-4 pt-16 mx-auto md:px-8 md:pt-24">
+    <div className="mx-auto w-full max-w-screen-xl px-4 pt-16 md:px-8 md:pt-24">
       <div className="flex flex-col justify-center space-y-8 md:flex-row md:space-y-0 md:space-x-16">
         <div>
           <h2 className="mt-0 mb-2 text-sm font-normal text-slate-500 md:text-center">{content.frameworks.label}</h2>
-          <div className="flex flex-wrap items-center -mx-3">
+          <div className="-mx-3 flex flex-wrap items-center">
             {content.frameworks.items.map(({ logo, label, url, supported }, index) => (
               <Link key={index} href={url}>
-                <a className="flex items-center h-16 p-3 dark:brightness-150 dark:filter">
+                <a className="flex h-16 items-center p-3 dark:brightness-200 dark:filter">
                   <Tooltip.Root delayDuration={100}>
-                    <Tooltip.Trigger className="flex items-center h-full">
+                    <Tooltip.Trigger className="flex h-full items-center">
                       <Image
                         src={content.logosPath + logo.file}
                         alt={label}
@@ -91,7 +91,7 @@ export const Support: FC = () => {
                       className="rounded bg-gray-800 px-3 py-1.5 text-sm text-slate-100 shadow-xl shadow-white dark:bg-violet-200 dark:text-violet-900 dark:shadow-black"
                     >
                       {`${label}${supported ? '' : ' – Considering to add support.'}`}
-                      <Tooltip.Arrow className="mx-1 text-gray-800 fill-current dark:text-violet-200" />
+                      <Tooltip.Arrow className="mx-1 fill-current text-gray-800 dark:text-violet-200" />
                     </Tooltip.Content>
                   </Tooltip.Root>
                 </a>
@@ -103,10 +103,10 @@ export const Support: FC = () => {
           <h2 className="mt-0 mb-2 text-sm font-normal text-slate-500 md:text-center">
             {content.contentSources.label}
           </h2>
-          <div className="flex flex-wrap items-center -mx-3">
+          <div className="-mx-3 flex flex-wrap items-center">
             {content.contentSources.items.map(({ logo, label, supported, url }, index) => (
               <Link key={index} href={url}>
-                <a className="flex items-center h-16 p-3 dark:brightness-150 dark:filter">
+                <a className="flex h-16 items-center p-3 dark:brightness-200 dark:filter">
                   <Tooltip.Root delayDuration={100}>
                     <Tooltip.TooltipTrigger>
                       <Image
@@ -128,7 +128,7 @@ export const Support: FC = () => {
                           ? '- Planned'
                           : ' – Considering to add support.'
                       }`}
-                      <Tooltip.Arrow className="mx-1 text-gray-800 fill-current dark:text-violet-200" />
+                      <Tooltip.Arrow className="mx-1 fill-current text-gray-800 dark:text-violet-200" />
                     </Tooltip.Content>
                   </Tooltip.Root>
                 </a>
