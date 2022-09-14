2f6965c6055f9067e1f193ad652e1e693eb228c2
Lukas Wiesehan <45076462+lukaswiesehan@users.noreply.github.com>
2022-09-14 13:27:22 +0200

    Update ChevronLink to not use next/link for external links
    Resolves #71

--- src/components/common/ChevronLink.tsx
+++ src/components/common/ChevronLink.tsx
@@ -5,11 +5,12 @@ import { Icon } from './Icon'
 const isExternalUrl = (link: string): boolean => !link.startsWith('/')
 
 export const ChevronLink: FC<{ label: string; url: string }> = ({ label, url }) => {
-  return (
-    <Link href={url}>
+  if (isExternalUrl(url)) {
+    return (
       <a
-        target={isExternalUrl(url) ? '_blank' : undefined}
-        rel={isExternalUrl(url) ? 'noreferrer' : undefined}
+        href={url}
+        target="_blank"
+        rel="noreferrer"
         className="inline-flex items-center space-x-1.5 text-violet-600 no-underline hover:text-violet-700 dark:text-violet-400 dark:hover:text-violet-300"
       >
         <span className="font-semibold">{label}</span>
@@ -17,6 +18,17 @@ export const ChevronLink: FC<{ label: string; url: string }> = ({ label, url }) 
           <Icon name="chevron-right" />
         </span>
       </a>
-    </Link>
-  )
+    )
+  } else {
+    return (
+      <Link href={url}>
+        <a className="inline-flex items-center space-x-1.5 text-violet-600 no-underline hover:text-violet-700 dark:text-violet-400 dark:hover:text-violet-300">
+          <span className="font-semibold">{label}</span>
+          <span className="block w-2">
+            <Icon name="chevron-right" />
+          </span>
+        </a>
+      </Link>
+    )
+  }
 }
