f429cf03fe8107726313382586e062f44e37f039
Sean C Davis <scdavis41@gmail.com>
2023-05-02 16:31:58 -0400

    Run prettier formatter

--- next.config.js
+++ next.config.js
@@ -5,11 +5,13 @@ module.exports = withContentlayer({
   images: {
     domains: ['pbs.twimg.com', 'avatars.githubusercontent.com', 'i.imgur.com'],
   },
-  headers: async () => [{
-    source: '/:path*',
-    headers: [
-      { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
-      { key: 'Cross-Origin-Embedder-Policy', value: 'same-origin' },
-    ],
-  }],
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+        { key: 'Cross-Origin-Embedder-Policy', value: 'same-origin' },
+      ],
+    },
+  ],
 })
